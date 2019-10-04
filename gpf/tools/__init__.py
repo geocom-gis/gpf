95f8eb4df94def1405104e14180eaa0b18f03962
Sander Schaminée <8972335+GeoSander@users.noreply.github.com>
2019-10-04 19:25:43 +0200

    Raise proper errors when importing arcpy fails instead of hiding them

--- gpf/tools/__init__.py
+++ gpf/tools/__init__.py
@@ -23,7 +23,6 @@ created for a more user-friendly experience and/or better performance.
 """
 
 import sys as _sys
-from warnings import warn as _warn
 
 import gpf.common.const as _const
 
@@ -35,14 +34,18 @@ try:
     import arcpy
 except RuntimeError as e:
     if _NOT_INITIALIZED in str(e):
-        _warn('Failed to obtain an ArcGIS license for the {!r} module'.format(_const.ARCPY), ImportWarning)
-        arcpy = _const.EMPTY_OBJ
-    else:
-        raise
+        # If the rather obscure "RuntimeError: NotInitialized" error is thrown,
+        # raise an ImportError instead with a clear reason.
+        raise ImportError('Failed to obtain an ArcGIS license for the {!r} module'.format(_const.ARCPY))
+    # Reraise for all other RuntimeErrors
+    raise
 except ImportError:
     if _const.ARCPY not in _sys.modules:
-        _warn('Python interpreter at {!r} cannot find the {!r} module'.format(_sys.executable, _const.ARCPY),
-              ImportWarning)
-        arcpy = _const.EMPTY_OBJ
-    else:
-        raise
+        # If arcpy cannot be found in the system modules,
+        # raise an ImportError that tells the user which interpreter is being used.
+        # The user might have accidentally chosen a "vanilla" Python interpreter,
+        # instead of the ArcGIS Python distribution.
+        raise ImportError('Python interpreter at {!r} '
+                          'cannot find the {!r} module'.format(_sys.executable, _const.ARCPY))
+    # Reraise for other (unlikely) ImportErrors
+    raise
