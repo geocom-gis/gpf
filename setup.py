87fa86cd530dbe0f2e071777e134a0b60c5dd59e
Sander Schaminée <8972335+GeoSander@users.noreply.github.com>
2019-10-05 00:03:13 +0200

    Changed url (homepage) back to GitHub repo url

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         long_description=read('README.rst'),
         author='Geocom Informatik AG / VertiGIS, Burgdorf, Switzerland',
         author_email='github@geocom.ch',
-        url='https://geocom.ch/',
+        url='https://github.com/geocom-gis/gpf',
         project_urls={
             'Source': 'https://github.com/geocom-gis/gpf',
             'Documentation': 'https://gpf.readthedocs.io/'
