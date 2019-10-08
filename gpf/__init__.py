0d3bd4e6ad850bffa59dbdd953dab10e440632ed
Sander Schaminee <8972335+GeoSander@users.noreply.github.com>
2019-10-08 23:13:14 +0200

    Moved arcpy import from tools subpackage to gpf

--- gpf/__init__.py
+++ gpf/__init__.py
@@ -1,6 +1,18 @@
 # coding: utf-8
+#
+# Copyright 2019 Geocom Informatik AG / VertiGIS
 
-# Copyright (c) 2019 | MIT License | Geocom Informatik AG, Burgdorf, Switzerland
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 """
 This is the documentation for the **gpf** (*Geocom Python Framework*) package for **Python 2.7**.
@@ -8,10 +20,42 @@ For the Python 3 version (suitable for ArcGIS Pro), please refer to the **gpf3**
 
 The *gpf* package contains several subpackages with tools and helpers for all kinds of
 ArcGIS-related geoprocessing and data management tasks.
-It is released under the MIT license as an open-source product,
+It is released under the Apache License 2.0 as an open-source product,
 allowing the community to freely use it, improve it and possibly add new features.
 
 Several tools in this package require Esri's ``arcpy`` Python library, which does not make this a *free* package.
 However, users who have already installed and authorized ArcGIS Desktop (ArcMap, ArcCatalog etc.) should be able to work
 with this package without any problems.
+
+.. note::   It is recommended to import ``arcpy`` via the ``gpf`` package (``from gpf import arcpy``).
+            This will load the same (and unmodified) module as ``import arcpy`` would load, but it shows
+            more useful error messages when the import fails.
 """
+
+import sys as _sys
+
+import gpf.common.const as _const
+
+_NOT_INITIALIZED = 'NotInitialized'
+
+
+try:
+    # Import the arcpy module globally
+    import arcpy
+except RuntimeError as e:
+    if _NOT_INITIALIZED in str(e):
+        # If the rather obscure "RuntimeError: NotInitialized" error is thrown,
+        # raise an ImportError instead with a clear reason.
+        raise ImportError('Failed to obtain an ArcGIS license for the {!r} module'.format(_const.ARCPY))
+    # Reraise for all other RuntimeErrors
+    raise
+except ImportError:
+    if _const.ARCPY not in _sys.modules:
+        # If arcpy cannot be found in the system modules,
+        # raise an ImportError that tells the user which interpreter is being used.
+        # The user might have accidentally chosen a "vanilla" Python interpreter,
+        # instead of the ArcGIS Python distribution.
+        raise ImportError('Python interpreter at {!r} '
+                          'cannot find the {!r} module'.format(_sys.executable, _const.ARCPY))
+    # Reraise for other (unlikely) ImportErrors
+    raise
--- gpf/tools/__init__.py
+++ gpf/tools/__init__.py
@@ -20,36 +20,4 @@ should make it a little easier to work with ArcGIS and ``arcpy``.
 
 Some classes are wrappers for well-known ``arcpy`` classes,
 created for a more user-friendly experience and/or better performance.
-
-.. note::   It is recommended to import ``arcpy`` from the *tools* subpackage (``from gpf.tools import arcpy``).
-            This will load the same (and unmodified) module as ``import arcpy`` would load, but it shows
-            more useful error messages when the import failed.
 """
-
-import sys as _sys
-
-import gpf.common.const as _const
-
-_NOT_INITIALIZED = 'NotInitialized'
-
-
-try:
-    # Import the arcpy module globally
-    import arcpy
-except RuntimeError as e:
-    if _NOT_INITIALIZED in str(e):
-        # If the rather obscure "RuntimeError: NotInitialized" error is thrown,
-        # raise an ImportError instead with a clear reason.
-        raise ImportError('Failed to obtain an ArcGIS license for the {!r} module'.format(_const.ARCPY))
-    # Reraise for all other RuntimeErrors
-    raise
-except ImportError:
-    if _const.ARCPY not in _sys.modules:
-        # If arcpy cannot be found in the system modules,
-        # raise an ImportError that tells the user which interpreter is being used.
-        # The user might have accidentally chosen a "vanilla" Python interpreter,
-        # instead of the ArcGIS Python distribution.
-        raise ImportError('Python interpreter at {!r} '
-                          'cannot find the {!r} module'.format(_sys.executable, _const.ARCPY))
-    # Reraise for other (unlikely) ImportErrors
-    raise
